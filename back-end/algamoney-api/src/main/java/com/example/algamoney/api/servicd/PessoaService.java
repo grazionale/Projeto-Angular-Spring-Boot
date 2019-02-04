8e1745be6d97bca52b33bf7b38a052e5c4cf03d1
grazionale <gabrielgrazionale@hotmail.com>
2019-02-04 16:24:51 -0200

    Atualizar Status(Ativo) de Pessoa

--- back-end/algamoney-api/src/main/java/com/example/algamoney/api/servicd/PessoaService.java
+++ back-end/algamoney-api/src/main/java/com/example/algamoney/api/servicd/PessoaService.java
@@ -14,11 +14,24 @@ public class PessoaService {
 	@Autowired
 	private PessoaRepository pessoaRepository;
 	public Pessoa atualizar(Pessoa pessoa, Long codigo) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return pessoaRepository.save(pessoaSalva);
+	}
+	
+	public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo) {
+		Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+		pessoaSalva.setAtivo(ativo);
+		pessoaRepository.save(pessoaSalva);
+	}
+	
+	private Pessoa buscarPessoaPeloCodigo(Long codigo) {
 		Pessoa pessoaSalva = pessoaRepository.findById(codigo).get();
 		if(pessoaSalva == null) {
 			throw new EmptyResultDataAccessException(1);
 		}
-		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
-		return pessoaRepository.save(pessoaSalva);
+		return pessoaSalva;
 	}
+	
+	
 }

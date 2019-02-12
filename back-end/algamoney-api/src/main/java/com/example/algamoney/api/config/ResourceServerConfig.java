7f5147fbf2e2e2673340de705553cbde232972ad
Gabriel Grazionale <gabrielgrazionale@hotmail.com>
2019-02-12 21:15:43 -0200

    Back-end - Segurança com OAuth e Passowrd Flow - Parte 1

--- back-end/algamoney-api/src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
+++ back-end/algamoney-api/src/main/java/com/example/algamoney/api/config/ResourceServerConfig.java
@@ -1,21 +1,27 @@
 package com.example.algamoney.api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	
 	//Configura usuário, senha para uma autenticação básica e passa uma regra. 
-	@Override
+	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
 			.withUser("admin").password(passwordEncoder().encode("admin")).roles("ROLE");
@@ -23,19 +29,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	//Informa que todas as requisições precisam passar por autenticação, exceto categorias.
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/categorias").permitAll()
 			.anyRequest().authenticated().and()
-			.httpBasic().and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and() //API não irá manter nenhum estado
 			.csrf().disable();	//Cross side request for forgery, é basicamente para desabilitar um javascrit injection na aplicação. Como é apenas uma API, então podemos desabilitar. 
-			
+	}
+	
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.stateless(true);
 	}
 	
 	@Bean
 	static PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 }

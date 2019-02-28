8b21b77a304ed309338beb030d84d0db85b6c810
Gabriel Grazionale <gabrielgrazionale@hotmail.com>
2019-02-28 00:52:42 -0300

    Frontend - Iniciando Projeto + PrimeNG

--- front-end/grazionale-money-app/src/app/app.module.ts
+++ front-end/grazionale-money-app/src/app/app.module.ts
@@ -4,13 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {TabViewModule} from 'primeng/components/tabview/tabview';
+import {ButtonModule} from 'primeng/components/button/button';
+import {InputTextModule} from 'primeng/components/inputtext/inputtext';
+import { DataTableModule } from 'primeng/components/datatable/datatable';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TabViewModule,
+    ButtonModule,
+    InputTextModule,
+    DataTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
